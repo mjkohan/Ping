1c8330371ffe63b3676cf1c7194dbd70b797b90a
mjkohan <mjk1551381@gmail.com>
2024-02-02 23:09:45 +0330

    added send icmp

--- ping.py
+++ ping.py
@@ -1,4 +1,6 @@
 import dns.resolver
+import socket
+import struct
 
 
 def ping(host, num_of_requests):
@@ -17,7 +19,30 @@ def check_host(host):
     except dns.resolver.NoAnswer:
         print(f"Error: No IP address found for {host}")
         return -1
+def send_icmp(host):
 
+    icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    icmp_socket.settimeout(1)
+    header = struct.pack('!BBHHH', 8, 0, 0, 111, 1)
+    data = b'Hello, ICMP!' 
+    icmp_checksum = calculate_checksum(header + data)
+    header = struct.pack('!BBHHH', 8, 0, icmp_checksum, 111, 1)
+    icmp_packet = header + data
+    icmp_socket.sendto(icmp_packet, (host, 0))
+    try:
+        response, _ = icmp_socket.recvfrom(1024)
+
+    except socket.timeout:
+        raise socket.error
+
+    icmp_socket.close()
+
+def calculate_checksum(data):
+    checksum = 0
+    for i in range(0, len(data), 2):
+        checksum += (data[i] << 8) + data[i+1]
+    checksum = (checksum >> 16) + (checksum & 0xFFFF)
+    return (~checksum) & 0xFFFF
 if __name__ == "__main__":
 
     host = input("Enter the domain / IP address : ")
