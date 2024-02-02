66ef4ace717b37292d25fa7caddac0292c95d31d
mjkohan <mjk1551381@gmail.com>
2024-02-02 22:29:18 +0330

    added get inputs

---
+++ ping.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+
+    target_host = input("Enter the domain or IP address to ping: ")
+    request_count = int(input("Enter the number of requests to make: "))
+    
+   
+
+
+    
